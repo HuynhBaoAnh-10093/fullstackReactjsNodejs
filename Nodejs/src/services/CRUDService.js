6055f28c4bbbb94c91e89788295db81a76706881
AnhHuynh1999 <74520649+AnhHuynh1999@users.noreply.github.com>
2023-05-28 18:34:49 +0700

    form add user

---
+++ Nodejs/src/services/CRUDService.js
@@ -0,0 +1,42 @@
+import bcrypt from "bcryptjs";
+import db from "../models/index";
+
+let salt = bcrypt.genSaltSync(10);
+
+let createNewUser = async (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+      console.log(data);
+      data.password = hashPasswordFromBcrypt;
+      await db.User.create({
+        email: data.email,
+        password: hashPasswordFromBcrypt,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        address: data.address,
+        phonenumber: data.phonenumber,
+        gender: data.gender === "1" ? true : false,
+        roleId: data.roleId,
+      });
+      resolve("ok created a new user successfully");
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+let hashUserPassword = (password) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let hashPassword = await bcrypt.hashSync(password, salt);
+      resolve(hashPassword);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+module.exports = {
+  createNewUser,
+};
