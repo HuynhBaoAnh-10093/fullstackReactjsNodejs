ec01f22164e6b05d8571f2c0e5506e5cddbdae74
anhhb <anhhb.blackwind@gmail.com>
2023-06-05 20:55:22 +0700

    read
    This reverts commit 20177f6922b7e04b8a47fd98997915fc9bd9a95e.

--- Nodejs/src/controllers/userController.js
+++ Nodejs/src/controllers/userController.js
@@ -23,7 +23,15 @@ let handleLogin = async (req, res) => {
 };
 
 let handleGetAllUsers = async (req, res, next) => {
-  let id = req.body.id; // All, id
+  let id = req.query.id; // All, id
+  console.log(id);
+  if (!id) {
+    return res.status(200).json({
+      errCode: 1,
+      errMessage: "Missing required parameter",
+      users: [],
+    });
+  }
   let users = await userService.getAllUsers(id);
   return res.status(200).json({
     errCode: 0,
--- ReactJs/src/containers/System/UserManage.js
+++ ReactJs/src/containers/System/UserManage.js
@@ -1,33 +1,72 @@
-import React, { Component } from 'react';
-import { FormattedMessage } from 'react-intl';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { FormattedMessage } from "react-intl";
+import { connect } from "react-redux";
+import "./UserManage.scss";
+import userService from "../../services/userService";
 class UserManage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    state = {
+  async componentDidMount() {
+    let req = await userService.geAllUsers("ALL");
+    console.log(req);
+  }
 
-    }
+  /**Life cyucle
+   * Run component:
+   * 1. run constructor -> init state
+   * 2. Did mount
+   * 3. Render
+   *
+   **/
 
-    componentDidMount() {
-
-    }
-
-
-    render() {
-        return (
-            <div className="text-center">Manage users</div>
-        );
-    }
-
+  render() {
+    return (
+      <div className="Users-container">
+        <div className="title text-center">Manage users</div>
+        <div className="users-table">
+          <table class="table">
+            <thead class="table-dark">
+              <tr>
+                <th>Email</th>
+                <th>Firstname</th>
+                <th>Lastname</th>
+                <th>Address</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>John</td>
+                <td>Doe</td>
+                <td>john@example.com</td>
+              </tr>
+              <tr>
+                <td>Mary</td>
+                <td>Moe</td>
+                <td>mary@example.com</td>
+              </tr>
+              <tr>
+                <td>July</td>
+                <td>Dooley</td>
+                <td>july@example.com</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-    };
+const mapStateToProps = (state) => {
+  return {};
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
